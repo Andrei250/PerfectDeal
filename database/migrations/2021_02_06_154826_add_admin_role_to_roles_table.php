80da5fceca04dad83dc3b31e2665b0643cba924d
Andrei Dumitrescu <andrei_dumitrescu2000@yahoo.ro>
2021-02-07 15:46:04 +0200

    Auth system (#4)
    * Added migrations and seeds
    
    * Added models
    
    * Added command to make a person admin
    
    * Added middleware for checking admin

---
+++ database/migrations/2021_02_06_154826_add_admin_role_to_roles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class AddAdminRoleToRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::table('roles')->insert([
+            ['name' => "Admin", "slug" => 'admin', "created_at" => Carbon::now()->toDateTimeString(), "updated_at" => Carbon::now()->toDateTimeString()]
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
