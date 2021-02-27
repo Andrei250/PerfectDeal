f9dfb3c7dfa7d0b2e8e1288746cae764a02e2b8e
Andrei250 <andrei_dumitrescu2000@yahoo.ro>
2021-02-27 15:57:42 +0200

    added my_orders page

--- resources/views/orders/newsfeed.blade.php
+++ resources/views/orders/newsfeed.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     @foreach($orders as $order)
-        @include('orders.order_component', ['order' => $order])
+        @include('orders.order_component', ['order' => $order, 'value' => 1])
     @endforeach
     {{$orders->links('vendor.pagination.bootstrap-4')}}
 
--- resources/views/user/my_orders.blade.php
+++ resources/views/user/my_orders.blade.php
@@ -1,5 +1,21 @@
 @extends('layouts.app')
 
-@section('contnet')
-    Hello There
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+                @forelse($orders as $order)
+                    <div class="col col-md-12">
+                        @include('orders.order_component', ['order' => $order, 'value' => 0])
+                    </div>
+                @empty
+                    <p class="alert alert-warning m-0 w-100">
+                        Nu aveti nicio comanda in sistem.
+                    </p>
+                @endforelse
+
+                @if(count($orders) > 0)
+                    {{$orders->links('vendor.pagination.bootstrap-4')}}
+                @endif
+        </div>
+    </div>
 @endsection
