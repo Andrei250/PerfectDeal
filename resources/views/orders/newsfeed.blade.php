ddaf3473427f9b8959ea2039a3d1e8c818b5a01b
Andrei Dumitrescu <andrei_dumitrescu2000@yahoo.ro>
2021-02-28 15:57:18 +0200

    Added input for search

--- resources/views/orders/newsfeed.blade.php
+++ resources/views/orders/newsfeed.blade.php
@@ -2,10 +2,26 @@
 
 @section('content')
 
-    @foreach($orders as $order)
-        @include('orders.order_component', ['order' => $order])
-    @endforeach
-    {{$orders->links('vendor.pagination.bootstrap-4')}}
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-7 mx-auto p-0 my-3">
+                <div class="form-outline">
+                    <input type="search" id="form1" class="form-control" placeholder="Cauta"/>
+                </div>
+            </div>
+        </div>
+
+        @foreach($orders as $order)
+            @include('orders.order_component', ['order' => $order])
+        @endforeach
+
+        <div class="row">
+            <div class="col-md-12">
+                {{$orders->links('vendor.pagination.bootstrap-4')}}
+            </div>
+        </div>
+    </div>
+
 
 
 @endsection
--- resources/views/orders/order_component.blade.php
+++ resources/views/orders/order_component.blade.php
@@ -1,5 +1,5 @@
 <div class="row my-2">
-    <div class="col col-md-7 bg-white rounded mx-auto">
+    <div class="col col-md-7 bg-white rounded mx-auto p-0">
         <div class="float-left col col-md-3 h-auto py-3">
             <img class="img-fluid" src="{{!is_null($order) ? $order->getImgPath() : asset('storage/uploads/orders/default_order.png')}}"/>
         </div>
