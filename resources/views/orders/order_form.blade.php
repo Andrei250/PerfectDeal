c92d6f0072302d8181006df76cb375f7d2a70fb1
Vlad Udrescu <43138612+VladUdri@users.noreply.github.com>
2021-03-05 12:16:51 +0200

    Modify announce new design (#14)
    * new design for change announce details
    
    * new style
    
    * comms added

--- resources/views/orders/order_form.blade.php
+++ resources/views/orders/order_form.blade.php
@@ -1,106 +1,137 @@
-<div class="form-group row align-items-center">
-    <label for="{{$title}}" class="col-md-3 col-form-label text-md-left">{{ __('Titlu') }}</label>
+<link href="{{asset('css/order_form_style.css')}}" rel="stylesheet">
 
-    <div class="col-md-9">
-        <input id="{{$title}}" type="text" class="form-control @error($title) is-invalid @enderror" name="{{$title}}"
-               value="{{ is_null(old($title)) ? $first_title : old($title) }}" required autofocus>
+<div>
+    <h3>Modifică anunț</h3>
+</div>
 
-        @error($title)
-        <span class="invalid-feedback" role="alert">
+<!-- Title -->
+<div class="bg-white rounded order-form px-5 py-3 my-3">
+    <div class="form-group row">
+        <label for="{{$title}}" class="col-md-10 col-form-label text-md-left">{{ __('Titlu') }}</label>
+
+        <div class="col-md-12">
+            <input id="{{$title}}" type="text" class="form-control @error($title) is-invalid @enderror"
+                   name="{{$title}}"
+                   value="{{ is_null(old($title)) ? $first_title : old($title) }}" required autofocus>
+
+            @error($title)
+            <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
             </span>
-        @enderror
+            @enderror
+        </div>
     </div>
 </div>
 
-<div class="form-group row align-items-center">
-    <label for="{{$description}}" class="col-md-3 col-form-label text-md-left">{{ __('Descriere') }}</label>
+<!-- Price -->
+<div class="bg-white rounded order-form px-5 py-3 my-3">
+    <div class="form-group row">
+        <label for="{{$price}}" class="col-md-10 col-form-label text-md-left">{{ __('Pret per unitate') }}</label>
 
-    <div class="col-md-9">
+        <div class="col-md-12">
+            <input id="{{$price}}" type="number" class="form-control @error($price) is-invalid @enderror"
+                   name="{{$price}}"
+                   value="{{ is_null(old($price)) ? $first_price : old($price) }}" required autofocus>
+
+            @error($price)
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+        </div>
+    </div>
+</div>
+
+<!-- Quantity & minimum quantity -->
+<div class="bg-white rounded order-form px-5 py-3 my-3">
+    <div class="form-group row mb-4">
+        <label for="{{$quantity}}" class="col-md-10 col-form-label text-md-left">{{ __('Cantitate') }}</label>
+
+        <div class="col-md-12">
+            <input id="{{$quantity}}" type="number" class="form-control @error($quantity) is-invalid @enderror"
+                   name="{{$quantity}}" value="{{ is_null(old($quantity)) ? $first_quantity : old($quantity) }}"
+                   required
+                   autofocus>
+
+            @error($quantity)
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+        </div>
+    </div>
+
+    <div class="form-group row">
+        <label for="{{$min_quantity}}"
+               class="col-md-10 col-form-label text-md-left">{{ __('Cantitate minima de cumparare') }}</label>
+
+        <div class="col-md-12">
+            <input id="{{$min_quantity}}" type="number"
+                   class="form-control @error($min_quantity) is-invalid @enderror"
+                   name="{{$min_quantity}}"
+                   value="{{ is_null(old($min_quantity)) ? $first_min_quantity : old($min_quantity) }}" required
+                   autofocus>
+
+            @error($min_quantity)
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+        </div>
+    </div>
+</div>
+
+<!-- Expiration date -->
+<div class="bg-white rounded order-form px-5 py-3 my-3">
+    <div class="form-group row">
+        <label for="{{$expire_date}}"
+               class="col-md-10 col-form-label text-md-left">{{ __('Data expirarii') }}</label>
+        <div class="col-md-12">
+            <input class="form-control @error($expire_date) is-invalid @enderror" type="date" id="{{$expire_date}}"
+                   name="{{$expire_date}}"
+                   value="{{is_null(old($expire_date)) ? $first_expire_date : old($expire_date)}}"
+                   required autofocus>
+
+            @error($expire_date)
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+            @enderror
+        </div>
+    </div>
+</div>
+
+<!-- Description -->
+<div class="bg-white rounded order-form px-5 py-3 my-3">
+    <div class="form-group row">
+        <label for="{{$description}}" class="col-md-10 col-form-label text-md-left">{{ __('Descriere') }}</label>
+
+        <div class="col-md-12">
         <textarea id="{{$description}}" type="text" class="form-control @error($description) is-invalid @enderror"
                   name="{{$description}}" required
                   autofocus>{{ is_null(old($description)) ? $first_description : old($description) }}</textarea>
 
-        @error($description)
-        <span class="invalid-feedback" role="alert">
+            @error($description)
+            <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
             </span>
-        @enderror
+            @enderror
+        </div>
     </div>
 </div>
 
-<div class="form-group row align-items-center">
-    <label for="{{$quantity}}" class="col-md-3 col-form-label text-md-left">{{ __('Cantitate') }}</label>
+<!-- Images -->
+<div class="bg-white rounded order-form px-5 py-5 my-3">
+    @if (isset($img_path))
+        <div >
+            Imagine curenta:<br>
+            <img src="{{$img_path}}" class="img-fluid col-md-10 col-form-label text-md-left"/>
+        </div>
 
-    <div class="col-md-9">
-        <input id="{{$quantity}}" type="number" class="form-control @error($quantity) is-invalid @enderror"
-               name="{{$quantity}}" value="{{ is_null(old($quantity)) ? $first_quantity : old($quantity) }}" required
-               autofocus>
+    @endif
 
-        @error($quantity)
-        <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
-            </span>
-        @enderror
+    <div class="form-control custom-file">
+        <input type="file" class="custom-file-input" id="{{$icon}}" name="{{$icon}}">
+        <label class="custom-file-label" for="{{$icon}}">Poza</label>
     </div>
-</div>
-
-<div class="form-group row align-items-center">
-    <label for="{{$min_quantity}}"
-           class="col-md-3 col-form-label text-md-left">{{ __('Cantitate minima de cumparare') }}</label>
-
-    <div class="col-md-9">
-        <input id="{{$min_quantity}}" type="number" class="form-control @error($min_quantity) is-invalid @enderror"
-               name="{{$min_quantity}}"
-               value="{{ is_null(old($min_quantity)) ? $first_min_quantity : old($min_quantity) }}" required autofocus>
-
-        @error($min_quantity)
-        <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
-            </span>
-        @enderror
-    </div>
-</div>
-
-<div class="form-group row align-items-center">
-    <label for="{{$price}}" class="col-md-3 col-form-label text-md-left">{{ __('Pret per unitate') }}</label>
-
-    <div class="col-md-9">
-        <input id="{{$price}}" type="number" class="form-control @error($price) is-invalid @enderror" name="{{$price}}"
-               value="{{ is_null(old($price)) ? $first_price : old($price) }}" required autofocus>
-
-        @error($price)
-        <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
-            </span>
-        @enderror
-    </div>
-</div>
-
-<div class="form-group row align-items-center">
-    <label for="{{$expire_date}}" class="col-md-3 col-form-label text-md-left">{{ __('Data expirarii') }}</label>
-    <div class="col-md-9">
-        <input class="form-control @error($expire_date) is-invalid @enderror" type="date" id="{{$expire_date}}"
-               name="{{$expire_date}}" value="{{is_null(old($expire_date)) ? $first_expire_date : old($expire_date)}}"
-               required autofocus>
-
-        @error($expire_date)
-        <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
-            </span>
-        @enderror
-    </div>
-</div>
-
-@if (isset($img_path))
-    <div class="col col-md-4">
-        Imagine curenta:<br>
-        <img src="{{$img_path}}" class="img-fluid"/>
-    </div>
-
-@endif
-
-<div class="custom-file">
-    <input type="file" class="custom-file-input" id="{{$icon}}" name="{{$icon}}">
-    <label class="custom-file-label" for="{{$icon}}">Poza</label>
 </div>
