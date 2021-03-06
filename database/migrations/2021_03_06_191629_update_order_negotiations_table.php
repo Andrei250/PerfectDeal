ab182cc0049ff4d70bfbb93ca576d89d19011bc2
Andrei250 <andrei_dumitrescu2000@yahoo.ro>
2021-03-06 21:30:54 +0200

    Added 2 migrations

---
+++ database/migrations/2021_03_06_191629_update_order_negotiations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateOrderNegotiationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('order_negotiations', function (Blueprint $table) {
+            $table->unsignedBigInteger('auth_id');
+
+            $table->foreign('auth_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('order_negotiations', function (Blueprint $table) {
+            $table->dropForeign(['auth_id']);
+            $table->dropColumn('auth_id');
+        });
+    }
+}
